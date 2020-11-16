8ce929cf877a3a176ac1dafd5621b9b5b7e1e38b
TJ Janus <TJJanus1@gmail.com>
2020-11-16 18:28:16 -0500

    added prod env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./api/server');
 
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
