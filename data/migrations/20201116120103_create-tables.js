77fa0e8085c5b9ff58234dfcd4c0f465b374e314
TJ Janus <TJJanus1@gmail.com>
2020-11-17 14:42:46 -0500

    description added

--- data/migrations/20201116120103_create-tables.js
+++ data/migrations/20201116120103_create-tables.js
@@ -14,6 +14,7 @@ exports.up = function(knex) {
     tbl.string('address', 200).notNullable()
     tbl.string('dates', 200).notNullable()
     tbl.string('guests', 200).notNullable()
+    tbl.string('description', 300)
     tbl.boolean('created', 200).notNullable()
     tbl.integer('users_id', 200)
     
--- data/seeds/02-events.js
+++ data/seeds/02-events.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('events').insert([
-        {id: 1, event_name: 'Fairmount Park Meet-Up', time: '12:00pm', address: 'Reservoir Dr, Philadelphia, PA 19119', dates: '11-20-20', guests: 'TJ, Alden, Jake, Cory', created: true, users_id: 1 },
-        {id: 2, event_name: 'Thanksgiving Food Drive 2020', time: '10:00am', address: 'Phoenix, Arizona', dates: '11-20-20', guests: 'TJ, Alden, Jake, Cory', created: true, users_id: 2 },
+        {id: 1, event_name: 'Fairmount Park Meet-Up', time: '12:00pm', address: 'Reservoir Dr, Philadelphia, PA 19119', dates: '11-20-20', guests: 'TJ, Alden, Jake, Cory', description: 'Fairmount Park is the largest municipal park in Philadelphia', created: true, users_id: 1 },
+        {id: 2, event_name: 'Thanksgiving Food Drive 2020', time: '10:00am', address: 'Phoenix, Arizona', dates: '11-20-20', guests: 'TJ, Alden, Jake, Cory', description: 'The safest way to celebrate Thanksgiving this year is to celebrate with people in your household.', created: true, users_id: 2 },
       ]);
     });
 };
